b62b3222acfc4063525dac1f0727351c1c89a487
Justin Garner <j.garner828@gmail.com>
2022-01-10 14:55:41 -0500

    created employee and engineer tests. to be completed later

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,10 +3,31 @@ const Employee = require('../lib/Employee');
 
 describe('Employee Class', () => {
 
-    it("it should allow you to instantiate a new Employee object", () => {
+    it("should allow you to instantiate a new Employee object", () => {
         const newObject = new Employee();
 
         expect(typeof(newObject)).toBe('object');
+    })
+
+    it("should allow you to add a name", () => {
+        const newName = 'Test Name';
+        const newObject = new Employee(newName);
+
+        expect(newObject.name).toBe(newName);
+    })
+
+    it("should allow you to add an id", () => {
+        const newId = 5;
+        const newObject = new Employee('Test Name', newId);
+
+        expect(newObject.id).toBe(newId);
+    })
+
+    it("should allow you to add an email", () => {
+        const newEmail = 'testemail@email.com';
+        const newObject = new Employee('Test Name', 4, newEmail);
+
+        expect(newObject.email).toBe(newEmail);
     })
 
 })
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,17 @@
 // test for Engineer.js
+
+const Engineer = require('../lib/Engineer');
+const Employee = require('../lib/Employee');
+
+describe('Engineer Class', () => {
+
+    it("should allow you to instantiate a new Engineer object", () => {
+        const newObject = new Engineer();
+
+        expect(typeof(newObject)).toBe('object');
+    })
+
+
+
+
+})
