1ca50322ab280e3e15fe833b8cc5641a8d3b3aab
Justin Garner <j.garner828@gmail.com>
2022-01-11 12:32:01 -0500

    started inquirer prompts to add new team member in index.js

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Team-Profile-Generator
+# Team Profile Generator
+[![MITLicense](https://img.shields.io/badge/License-MITLicense-<COLOR>.svg)](https://shields.io/)
+  
+
+## Contributors
+*Justin Garner*
+
+## Repo
+**https://github.com/jgarner828/Team-Profile-Generator**
+
+  
+# Project Description
+  
+**This is a Node.js command-line app to create a team profile. Using command line entries, user is able to add team members with different queries based on the position held by each team member and render a web page with each team member.**
+
+## Usage
+
+**Install required Node modules. In terminal run the command 'node index.js'.**
+  
+## License
+  
+### [MIT License Link](https://opensource.org/licenses/MIT)
+  
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const Inquirer = require('inquirer');
+
+
+// TODO: need to select what type of employee you are going to add. need to generate the list of questions based on what you select. need to save the employee to an array of employees. need to determine if you want to add more employees (show current list), then when you submit the list of employees is renders a page to the /dist directory
+
+const employeeRole = { 
+                    type: 'list',
+                    name: 'employeeRole',
+                    message: "What type of employee would you like to add?",
+                    choices: ['Manager', 'Engineer', 'Intern']
+                }
+
+Inquirer
+    .prompt(employeeRole)
+
+    .then( (response) => { 
+        init(response.employeeRole);
+        return;
+    })
+
+    .catch( (error) => {
+        console.log(error);
+    })
+
+
+function init(response) {
+    console.log(response);
+}
