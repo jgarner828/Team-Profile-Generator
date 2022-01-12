75ebdbf7baa63470a1099602a73e9be64f9b9dd4
Justin Garner <j.garner828@gmail.com>
2022-01-12 12:21:01 -0500

    started work on parsePage.js by filtering parameters brought in

--- parsePage.js
+++ parsePage.js
@@ -1,7 +1,22 @@
 function parsePage(managers, engineers, interns) {
-    console.log(managers);
-    console.log(engineers);
-    console.log(interns);
+
+    if(managers.length > 0) {
+        for(let i = 0; i < managers.length; i++){ 
+            console.log(managers[i].name);
+        }
+    }
+  
+    if(engineers.length > 0) {
+        for(let i = 0; i < engineers.length; i++){ 
+            console.log(engineers[i].name);
+        }
+    }
+
+    if(interns.length > 0) {
+        for(let i = 0; i < interns.length; i++){ 
+            console.log(interns[i].name);
+        }
+    }    
 
 }
 
